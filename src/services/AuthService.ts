d0774bdcfd9ff7cda73f3f0b7329452c3163a7bc
Renê <renejr.arraes286@gmail.com>
2020-04-11 19:29:10 -0300

    Extraindo email do token

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { CredenciaisDto } from './../models/crendeciais.dto';
 import { Injectable } from "@angular/core";
 import { API_CONFIG } from '../config/api.config';
+import { JwtHelper } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService{
@@ -11,7 +12,7 @@ export class AuthService{
     constructor (public http : HttpClient, public storage : StorageService){
 
     }
-    
+    jwtHelper : JwtHelper = new JwtHelper();    
     authenticate (creds : CredenciaisDto){
 
         return this.http.post(`${API_CONFIG.base_url}/login`,
@@ -28,7 +29,8 @@ export class AuthService{
         let tok = authorizantionvalue.substring(7);
         
         let user : LocalUser = {
-            token : tok
+            token : tok,
+            email: this.jwtHelper.decodeToken(tok).sub
         };
         this.storage.setLocalUser(user);
     }
